e3380f69e08e7fc731ca83cc3912d660e193925d
Aditya Shrivastava <64320843+srwaditya@users.noreply.github.com>
2021-02-12 16:08:06 +0530

    Update youtube_bot.py

--- venv/lib/youtube_bot.py
+++ venv/lib/youtube_bot.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from time import sleep
-driver1= webdriver.Chrome(executable_path="/Users/adityashrivastava/Downloads/my_project/chromedriver")
+driver1= webdriver.Chrome(executable_path="/Users/adityashrivastava/Downloads/my_project/chromedriver") ''' Here you put your chromedriver path location '''
 driver2= webdriver.Chrome(executable_path="/Users/adityashrivastava/Downloads/my_project/chromedriver")
 driver3= webdriver.Chrome(executable_path="/Users/adityashrivastava/Downloads/my_project/chromedriver")
 
-driver1.get("https://www.youtube.com/watch?v=vPIwFPEIJBg")
-driver2.get("https://www.youtube.com/watch?v=6-aO3XtI6os")
-driver2.get("https://www.youtube.com/watch?v=QLrKXerJAAg")
+driver1.get("")   '''-- Put here your video URL 1 '''
+driver2.get("")   '''-- Put here your video URL 2 '''
+driver2.get("")   '''-- Put here your video URL 3 '''
 
 while True:
     sleep(60)
